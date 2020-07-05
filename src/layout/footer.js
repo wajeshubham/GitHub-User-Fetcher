ec0c0380810607e7a9ca342bc8c0b77999707387
Shubham Waje <waje.shubham111@gmail.com>
2020-07-05 19:08:52 +0530

    added footer logic

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaGithub } from "react-icons/fa";
+
+import UserContext from "../context/UserContext";
 
 import { Container } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const context = useContext(UserContext);
   return (
     <Container
       fluid
       tag="footer"
       className="text-center bg-dark text-secondary p-3 fixed-bottom"
     >
-      GitHub Repository/user Search app
-      <span className="float-right text-white">
-        <a href="https://github.com/wajeshubham" style={{ color: "white" }}>
-          <FaGithub className="mr-2 mb-2" style={{ fontSize: "20px" }} />
-          Follow @wajeshubham
-        </a>
-      </span>
+      {context.user?.uid ? (
+        <span className="text-white mt-2">
+          <a href="https://github.com/wajeshubham" style={{ color: "white" }}>
+            <FaGithub className="mr-2 mb-2" style={{ fontSize: "20px" }} />
+            Follow @wajeshubham
+          </a>
+        </span>
+      ) : (
+        <p>GitHub user Search app</p>
+      )}
     </Container>
   );
 };
